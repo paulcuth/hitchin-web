8a17f140c723cc4ed64cb04f0d6333ef4c7d87a4
Paul Cuthbertson <paulcuthbertson@monzo.com>
2022-08-31 21:18:42 +0100

    Adds OG data, updates text.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,23 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Hitchin Web - A monthly meetup of Web folk in Hitchin.</title>
+        <title>
+          Hitchin Web - A friendly, informal meetup of Web folk in Hitchin.
+        </title>
         <meta
           name="description"
           content="Meetup group for any Hitchin folk who create for the Web."
         />
+        <meta property="og:title" content="Hitchin Web" />
+        <meta property="og:site_name" content="Hitchin Web" />
+        <meta property="og:url" content="https://hitchin-web.org/" />
+        <meta
+          property="og:description"
+          content="A friendly, informal meetup of Web folk in Hitchin."
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="https://hitchin-web.org/logo.png" />
+        <meta property="og:locale" content="en_GB" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
@@ -21,28 +33,30 @@ const Home: NextPage = () => {
         </h1>
 
         <section className={styles.detail}>
-          <p className={styles.interests}>
-            Front-end, back-end, UX, design, accessibility, and more.
+          <p className={styles.description}>
+            A friendly, informal meetup for anyone with an interest in the being
+            creative with the Web.
           </p>
 
-          <p className={styles.description}>
-            A friendly, informal monthly meetup for anyone with an interest in
-            the being creative with the Web.
+          <p className={styles.interests}>
+            Front-end, back-end, design, UX, accessibility & more.
           </p>
 
           <p className={styles.eventInfo}>
             <strong>7pm</strong> onwards @{" "}
-            <strong>The Cooper&apos;s Arms</strong>, 81 Tilehouse Street.
+            <strong>The&nbsp;Cooper&apos;s&nbsp;Arms</strong>,
+            81&nbsp;Tilehouse&nbsp;Street.
             <br />
-            <strong>21 September</strong> & <strong>19 October 2022</strong>.
+            <strong>21&nbsp;September</strong> &{" "}
+            <strong>19&nbsp;October</strong> 2022.
           </p>
 
           <p className={styles.description}>
-            See{" "}
+            For latest information and to get in touch, check out{" "}
             <a href="https://twitter.com/hitchin_web">
               @hitchin_web on Twitter
-            </a>{" "}
-            for latest information.
+            </a>
+            .
           </p>
         </section>
       </main>
